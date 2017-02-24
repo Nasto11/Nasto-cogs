b92ae88624a6494e1e6a3b379e082a2c4c41b90f
Nasto11 <kubacz1132@outlook.com>
2017-02-24 17:52:26 +0100

    indentation fix

--- tak/tak.py
+++ tak/tak.py
@@ -7,38 +7,39 @@ import os
 class Music:
     """Changes nickname when command is used"""
 
-def __init__(self, bot):
-    self.bot = bot
-    self.setowner_lock = False
+    def __init__(self, bot):
+        self.bot = bot
+        self.setowner_lock = False
 				
-  @commands.group(pass_context=True)		
-  @checks.is_owner()
- async def music(self, ctx):
-    if ctx.invoked_subcommand is None:
-        await send_cmd_help(ctx)
+    @commands.group(pass_context=True)		
+    @checks.is_owner()
+    async def music(self, ctx):
+       if ctx.invoked_subcommand is None:
+           await send_cmd_help(ctx)
 
-  @music.command(pass_context=True)
-  @checks.is_owner()
- async def _on(self, ctx, *, nickname=""):
-    nickname = nickname.strip()
-    if nickname == "Dank Bot |Music on voice!":
-        nickname = "Dank Bot |Music on voice!"
-    try:
-        await self.bot.change_nickname(ctx.message.server.me, nickname)
-    except discord.Forbidden:
-        await self.bot.say("I cannot do that, I miss the `Change Nickname` permission")
+    @music.command(pass_context=True)
+    @checks.is_owner()
+    async def _on(self, ctx, *, nickname=""):
+	    """Adds "|Music on voice!" to the nickname"""
+        nickname = nickname.strip()
+        if nickname == "Dank Bot |Music on voice!":
+            nickname = "Dank Bot |Music on voice!"
+        try:
+            await self.bot.change_nickname(ctx.message.server.me, nickname)
+        except discord.Forbidden:
+            await self.bot.say("I cannot do that, I miss the `Change Nickname` permission")
 	
-  @music.command(pass_context=True)	
-  @checks.is_owner()	
- async def _off(self, ctx, *, nickname=""):
-     nickname = nickname.strip()
-    if nickname == "":
-        nickname = None
-    try:
-        await self.bot.change_nickname(ctx.message.server.me, nickname)
-    except discord.Forbidden:
-        await self.bot.say("I cannot do that, I miss the `Change Nickname` permission")
+    @music.command(pass_context=True)	
+    @checks.is_owner()	
+    async def _off(self, ctx, *, nickname=""):
+         nickname = nickname.strip()
+        if nickname == "":
+            nickname = None
+        try:
+            await self.bot.change_nickname(ctx.message.server.me, nickname)
+        except discord.Forbidden:
+            await self.bot.say("I cannot do that, I miss the `Change Nickname` permission")
 
- def setup(bot):
-    bot.add_cog(Music)
+    def setup(bot):
+        bot.add_cog(Music)
 
