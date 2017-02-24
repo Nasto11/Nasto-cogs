9673de96f8083c22985adc9196e523ac5f318ee3
Nasto11 <kubacz1132@outlook.com>
2017-02-24 15:38:22 +0100

    fixx

--- tak/tak.py
+++ tak/tak.py
@@ -1,19 +1,31 @@
 import discord
 from discord.ext import commands
-from .utils.dataIO import fileIO
-from .utils import checks
-from __main__ import send_cmd_help
+from cogs.utils import checks
+from __main__ import set_cog
+from .utils.dataIO import dataIO
+from .utils.chat_formatting import pagify, box
+
+import importlib
+import traceback
+import logging
+import asyncio
+import threading
+import datetime
+import glob
 import os
-import time
+import aiohttp
 
 
 class Musict:
-    """Cycles random statuses
-    If a custom status is already set, it won't change it until
-    it's back to none. (!set status)"""
+    """Changes nickname when command is used"""
 
 def __init__(self, bot):
         self.bot = bot
+        self.setowner_lock = False
+        self.file_path = "data/red/disabled_commands.json"
+        self.disabled_commands = dataIO.load_json(self.file_path)
+        self.session = aiohttp.ClientSession(loop=self.bot.loop)
+
 		
 @commands.group(pass_context=True)		
 @checks.is_owner()
@@ -25,7 +37,7 @@ async def music(self, ctx):
 @checks.is_owner()
 async def _on(self, ctx, *, nickname=""):
         nickname = nickname.strip()
-        if nickname == "":
+        if nickname == "Dank Bot |Music on voice!":
             nickname = "Dank Bot |Music on voice!"
         try:
             await self.bot.change_nickname(ctx.message.server.me, nickname)
