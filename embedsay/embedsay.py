ae7d4b990f543794de1f94a71007347a1440a676
Nasto11 <kubacz1132@outlook.com>
2017-03-04 22:56:29 +0100

    autism

--- embedsay/embedsay.py
+++ embedsay/embedsay.py
@@ -10,16 +10,9 @@ class Embedsay:
 
     @commands.command()
     async def embedsay(self):
-        """Says stuff!"""
-        if author.id in self.data:
-                    avatar = author.avatar_url if author.avatar else author.default_avatar_url
-                    if self.data[author.id][text]:
-                        em = discord.Embed(description=self.data[author.id][text], color=discord.Color.blue())
-                        em.set_author(name='{} says'.format(author.display_name), icon_url=avatar)
-                    else:
-                        em = discord.Embed(color=discord.Color.blue())
-                        em.set_author(name='{} says'.format(author.display_name), icon_url=avatar)
-                    await self.bot.send_message(message.channel, embed=em)
+    """Says stuff!"""
+        embed=discord.Embed(title="Im saying", url='http://youhaveautism.com/', description="that u have autism ", color=0x0080ff)
+        await self.bot.say(embed=embed)
 
 def setup(bot):
     bot.add_cog(Embedsay(bot))
