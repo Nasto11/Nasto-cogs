4db70cef8b16a69c64013fa8c8ee5c0fa8075567
Nasto11 <kubacz1132@outlook.com>
2017-03-04 23:01:28 +0100

    fixxx

--- embedsay/embedsay.py
+++ embedsay/embedsay.py
@@ -8,11 +8,13 @@ class Embedsay:
     def __init__(self, bot):
         self.bot = bot
 
-    @commands.command()
+    @commands.command(pass_context=True)
     async def autism(self):
         """Says stuff!"""
         embed=discord.Embed(title="Im saying", url='http://youhaveautism.com/', description="that u have autism ", color=0x0080ff)
+		
         await self.bot.say(embed=embed)
+		
 
 def setup(bot):
     bot.add_cog(Embedsay(bot))
