ed57f9f7b74fb1846793bd75fdef6db365cda80a
Nasto11 <kubacz1132@outlook.com>
2017-03-04 23:44:00 +0100

    update

--- embedsay/embedsay.py
+++ embedsay/embedsay.py
@@ -1,6 +1,6 @@
+import os
 import discord
 from discord.ext import commands
-from .utils.dataIO import dataIO
 
 class Embedsay:
     """Says stuff!"""
@@ -8,13 +8,15 @@ class Embedsay:
     def __init__(self, bot):
         self.bot = bot
 
-    @commands.command(pass_context=True)
-    async def autism(self):
+    @commands.command()
+    async def embedsay(self, message, ctx, *text):
         """Says stuff!"""
-        embed=discord.Embed(title="Im saying", url='http://youhaveautism.com/', description="that u have autism ", color=0x0080ff)
+
+        text = " ".join(text)
+        embed=discord.Embed(title="says", description=text, color=0xff00bb)
+        embed.set_author(name='{} says'.format(author.display_name), icon_url=avatar)
         await self.bot.delete_message(ctx.message)
         await self.bot.say(embed=embed)
-		
 
 def setup(bot):
-    bot.add_cog(Embedsay(bot))
+bot.add_cog(Embedsay(bot))
