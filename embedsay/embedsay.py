615f2d1e371a8cf9c2d4421b85982621af4a5e6e
Nasto11 <kubacz1132@outlook.com>
2017-03-05 01:21:04 +0100

    Last update4

--- embedsay/embedsay.py
+++ embedsay/embedsay.py
@@ -12,12 +12,11 @@ class Embedsay:
     async def embedsay(self, ctx, *, text):
         """Says stuff!"""
 
-        text = " ".join(text)
         author = ctx.message.author
-        embed=discord.Embed(description=text, color=0xff00bb)
-        embed.set_author(name='{} says', icon_url=author)
+        em=discord.Embed(description=text, color=0xff00bb)
+        em.set_author(name='{} says'.format(author), icon_url=author.avatar_url)
         await self.bot.delete_message(ctx.message)
-        await self.bot.say(embed=embed)
+        await self.bot.say(embed=em)
 
 def setup(bot):
     bot.add_cog(Embedsay(bot))
