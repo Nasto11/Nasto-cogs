92b028c5ae84654485bca6e44e5bea2fa834ae14
Nasto11 <kubacz1132@outlook.com>
2017-03-15 18:56:49 +0100

    total remake

--- approval/approval.py
+++ approval/approval.py
@@ -9,49 +9,18 @@ class Approval:
     """"""
 
     def __init__(self, bot):
-        self.bot = bot
-        self.default = "data/approval/defaultrole.json"
-        self.set = dataIO.load_json(self.defaultrole)
-        
-    @commands.command(pass_context=True)
-    @checks.admin_or_permissions(manage_server=True)
-    async def setdefaultrole(self, ctx, default_role):
-        server = ctx.message.server
-        count = 0
-        for role in server.roles:
-            if role.name.lower() == default_role.lower():
-                break
-            else:
-                count += 1
-
-        if count == len(server.roles):
-            await self.bot.say("Role does not exist.")
-            return
-        if server.id not in self.roleset:
-            self.set[server.id] = default_role
-            dataIO.save_json(self.default, self.set)
-        else:
-            self.set[server.id] = default_role
-            dataIO.save_json(self.default, self.set)
-        await self.bot.say("Changed the default *role* to {}!".format(default_role))
-
+        self.bot = bot        
 
     async def listener(self, ctx, message):
         author = ctx.message.author
-        default_role = self.set[server.id]
+        userrole = "Member"
         server = author.server
         channel = ctx.message.channel
         user = author
-        await self.bot.wait_for_message(author=author)
-            if msg.content.lower().strip() == "N0OB M3NU":
-                try:
-                    for role in server.roles:
-                        if role.name.lower() == default_role.lower():
-                            userrole = role
-                            break
-                    await self.bot.add_roles(author, userrole)
-                    await self.bot.delete_message(ctx.message)
-
+        if message.content == "N0OB M3NU": 
+            try:             
+                await self.bot.add_roles(author, userrole)
+                await self.bot.delete_message(ctx.message)					
     def setup(bot):
         n = approval(bot)
 	    bot.add_cog(n)
