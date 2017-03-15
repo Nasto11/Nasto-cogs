5f7ac7e71f94c66b21bf5f82b0a936787511a147
Nasto11 <kubacz1132@outlook.com>
2017-03-15 17:45:50 +0100

    :))

--- approval/approval.py
+++ approval/approval.py
@@ -3,7 +3,7 @@ import discord
 from discord.ext import commands
 import asyncio
 
-class Embedsay:
+class Approval:
     """Says stuff!"""
 
     def __init__(self, bot):
@@ -12,11 +12,11 @@ class Embedsay:
         author = ctx.message.author
 		if message.author.id !=self.bot.user.id:
 		    try:
-			    if message.content == "N0Ob M3NU":                
+			    if message.content == "N0OB M3NU":                
                     async for x in self.bot.logs_from(channel, before=message.timestamp, limit=1):
                         await self.bot.add_roles(author, Member) 
 
-def setup(bot):
-    n = approval(bot)
-	bot.add_cog(n)
-	bot.add_listener(n.listener, "on_message")
+    def setup(bot):
+        n = approval(bot)
+	    bot.add_cog(n)
+	    bot.add_listener(n.listener, "on_message")
