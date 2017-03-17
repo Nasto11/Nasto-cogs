a5613aec5dc2cbe8b406fa0751b6a4e0b78c9bec
Nasto11 <kubacz1132@outlook.com>
2017-03-17 22:53:42 +0100

    last update

--- approval/approval.py
+++ approval/approval.py
@@ -8,15 +8,20 @@ class Approval:
 
     def __init__(self, bot):
         self.bot = bot        
-
-    async def listener(self, ctx, message):
-        author = ctx.message.author
-        userrole = "Member"
+    async def listener(self, message):
+        author = message.author
+        server = author.server
+        rolename = discord.utils.get(server.roles, name="Member")
         if message.content == "N0OB M3NU": 
             try:             
-                await self.bot.add_roles(author, userrole)
-                await self.bot.delete_message(ctx.message)					
-    def setup(bot):
-        n = approval(bot)
-	    bot.add_cog(n)
-	    bot.add_listener(n.listener, "on_message")
+                await self.bot.add_roles(author, rolename)
+                await self.bot.delete_message(message)
+            except discord.Forbidden:
+                pass
+            except discord.HTTPException:
+                pass
+
+def setup(bot):
+    n = Approval(bot)
+    bot.add_listener(n.listener, 'on_message')
+    bot.add_cog(n)
